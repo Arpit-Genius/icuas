8a1265d380520b3366a1fe4bf4958e8d78a5d2d3
Arpit-Genius <arpitaps892@gmail.com>
2026-06-17 18:38:43 +0530

    Add files via upload

---
+++ startup/_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NUM_ROBOTS=5
+export KEYWORD=icuas26_1 #icuas26_1 or empty
+export ENV_NAME=${KEYWORD}_world
+export SPAWN_POSE_DOC=positions_${KEYWORD}.txt
+export GZ_VERSION=garden
+export BINVOX_STL_LOCATION=""
+export COMM_RANGE=70
+export AGV_VEL=0.5
+export CHARGING_FILE=charging_$KEYWORD.yaml
+export AGV_PATH=AGV_path_$KEYWORD.yaml
